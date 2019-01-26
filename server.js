8538384189e08915bfbb168f84c01414c1fa268a
Kazi Shoaib Muhammad <shoaib.iiuc.cse@gmail.com>
2019-01-26 09:20:26 +0600

    server.log issue

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use((req, res, next)=>{
 	let now = new Date().toString();
 	let log = `${now}: ${req.method} ${req.url}`;
 	console.log(log);
-	fs.appendFile('sever.log',log+'\n', (err)=>{
+	fs.appendFile('server.log',log+'\n', (err)=>{
 		if(err)
 			console.log('unable to append to server log file');
 	});
